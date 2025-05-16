52af3aa79903db72926f7b36822eb3e76bb9cd00
Rahel <rahelhabtamu68@gmail.com>
2025-05-16 16:30:42 +0300

    challende plus ai css in javascript

---
+++ plus AI workshop/indexs.js
@@ -0,0 +1,30 @@
+function handleClick()
+{
+
+buttonElement.setAttribute("disable", "disable");
+
+
+    
+    // let heading = document.querySelector("h1");
+    // class list
+    // heading.classList.add("highlight");
+}
+let buttonElement = document.querySelector("#red");
+buttonElement.addEventListener("click",handleClick);
+let buttonElement1 = document.querySelector("#red");
+buttonElement1.style.backgroundColor = "green";
+
+let buttonElement2 = document.querySelector("#style");
+buttonElement2.classList.add("danger");
+
+// attributes
+// innerHTML
+//setAttribute
+//element.style.padding = "10px";
+
+//        classList
+
+//element.classList.add("my")
+//element.classList.toggle("my")
+//element.classList.remove("my")
+
